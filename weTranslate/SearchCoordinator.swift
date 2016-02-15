265b5081a0346f31eebf7a1a6280117b9435ee7e
Lionel Alves <alves.lionel5@gmail.com>
2016-02-14 21:44:52 -0500

    Setup weTranslate project Put a `UITabBarController` as root Use coordinators to handle the navigation Display an empty search view controller for now

---
+++ weTranslate/SearchCoordinator.swift
@@ -0,0 +1,27 @@
+//
+//  SearchCoordinator.swift
+//  weTranslate
+//
+//  Created by Lionel on 2/14/16.
+//  Copyright © 2016 weTranslate. All rights reserved.
+//
+
+import UIKit
+
+final class SearchCoordinator: CoordinatorType {
+
+    let navigationController: UINavigationController
+    var childCoordinators: [CoordinatorType] = []
+
+    // MARK: - Initialization
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    // MARK: -
+
+    func start() {
+        navigationController.pushViewController(SearchViewController(), animated: false)
+    }
+}
---
+++ weTranslate/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.swift
+//  weTranslate
+//
+//  Created by Lionel on 2/14/16.
+//  Copyright © 2016 weTranslate. All rights reserved.
+//
+
+import UIKit
+
+final class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        view.backgroundColor = .whiteColor()
+    }
+}

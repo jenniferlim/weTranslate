57702f4484cff77ff5cc355aec43db3378254ff3
Lionel Alves <alves.lionel5@gmail.com>
2016-02-15 22:59:14 -0500

    Add code skeleton with FIXMEs for the search feature

---
+++ weTranslate/WordViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  MeaningViewModel.swift
+//  weTranslate
+//
+//  Created by Lionel on 2/15/16.
+//  Copyright © 2016 weTranslate. All rights reserved.
+//
+
+import Foundation
+//import TranslateKit
+
+struct WordViewModel {
+
+    // FIXME:
+    //    init(word: Word) {
+    //
+    //    }
+
+    let word: String = "bras nm"
+    let sense: String = "corps"
+}

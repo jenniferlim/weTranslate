109587042e26ee49c45dfa99709252fa4341f4da
Lionel Alves <alves.lionel5@gmail.com>
2016-02-21 01:05:08 -0500

    * Add `Color` class that will contain all the color used in the app

---
+++ weTranslate/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  weTranslate
+//
+//  Created by Lionel on 2/21/16.
+//  Copyright © 2016 weTranslate. All rights reserved.
+//
+
+import UIKit
+
+final class Color {
+
+    static let brand = UIColor(red: 0.961, green: 0.314, blue: 0.235, alpha: 1)
+}

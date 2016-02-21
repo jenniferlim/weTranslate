4b0d3371a758036d7797f23bdcc8f35a6060981d
Lionel Alves <alves.lionel5@gmail.com>
2016-02-21 01:32:16 -0500

    * The `Font` will contain all the fonts used in the app

---
+++ weTranslate/Font.swift
@@ -0,0 +1,14 @@
+//
+//  Font.swift
+//  weTranslate
+//
+//  Created by Lionel on 2/21/16.
+//  Copyright © 2016 weTranslate. All rights reserved.
+//
+
+import UIKit
+
+final class Font {
+
+    static let body = UIFont(name: "HelveticaNeue", size: 14)
+}

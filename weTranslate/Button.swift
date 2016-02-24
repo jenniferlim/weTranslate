a14286153251d16a7acef348c820832de0059392
Jennifer Lim <jennifer.lim17@gmail.com>
2016-02-23 23:18:11 -0500

    Add generic button and label

---
+++ weTranslate/Button.swift
@@ -0,0 +1,27 @@
+//
+//  Button.swift
+//  weTranslate
+//
+//  Created by Jennifer on 23/02/2016.
+//  Copyright © 2016 weTranslate. All rights reserved.
+//
+
+import UIKit
+
+final class Button: UIButton {
+    
+    // MARK: - Init
+    
+    init(title: String, color: UIColor) {
+        
+        super.init(frame: CGRectZero)
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        setTitle(title, forState: .Normal)
+        setTitleColor(color, forState: .Normal)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ weTranslate/Label.swift
@@ -0,0 +1,26 @@
+//
+//  Label.swift
+//  weTranslate
+//
+//  Created by Jennifer on 23/02/2016.
+//  Copyright © 2016 weTranslate. All rights reserved.
+//
+
+import UIKit
+
+final class Label: UILabel {
+
+    // MARK: - Init
+    
+    init(textColor: UIColor, font: UIFont?) {
+        super.init(frame: CGRectZero)
+        translatesAutoresizingMaskIntoConstraints = false
+        self.textColor = textColor
+        self.font = font
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}

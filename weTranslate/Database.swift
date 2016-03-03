3ae4bbe62fd59dc4bb833465a51470118bdfdcc2
Jennifer Lim <jennifer.lim17@gmail.com>
2016-03-03 00:30:09 -0500

    Renaming Store to Database and fix get/set items

--- weTranslate/Database.swift
+++ weTranslate/Database.swift
@@ -12,7 +12,7 @@ import TranslateKit
 struct Database<T where T:DictionaryDeserializable, T:DictionarySerializable> {
 
     private let dbFilePath: String
-
+    private let queue = dispatch_queue_create("com.fluen.fluenapp.Database", DISPATCH_QUEUE_CONCURRENT)
     init?(dbFileName: String) {
         guard let documentsDirectory = (NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)).first else { return nil }
         dbFilePath = (documentsDirectory as NSString).stringByAppendingPathComponent(dbFileName)
@@ -20,7 +20,7 @@ struct Database<T where T:DictionaryDeserializable, T:DictionarySerializable> {
 
     func get() -> [T] {
         var items = [T]()
-        dispatch_sync(dispatch_get_main_queue()) {
+        dispatch_sync(queue) {
             guard let data = NSData(contentsOfFile: self.dbFilePath),
                 objects = try? NSJSONSerialization.JSONObjectWithData(data, options: []),
                 dictionaries = objects as? [JSONDictionary] else { return }
@@ -30,9 +30,10 @@ struct Database<T where T:DictionaryDeserializable, T:DictionarySerializable> {
     }
 
     func set(items: [T]) {
-
-        let dictionaries = items.map { $0.dictionary }
-        guard let data = try? NSJSONSerialization.dataWithJSONObject(dictionaries, options: []) else { return }
-        data.writeToFile(self.dbFilePath, atomically: true)
+        dispatch_barrier_async(queue) {
+            let dictionaries = items.map { $0.dictionary }
+            guard let data = try? NSJSONSerialization.dataWithJSONObject(dictionaries, options: []) else { return }
+            data.writeToFile(self.dbFilePath, atomically: true)
+        }
     }
 }
